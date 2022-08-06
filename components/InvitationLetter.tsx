75a748e79af1bf4d503695a4e8b0cf4ff82c83fc
Hoon Wee <mrgravity817@gmail.com>
2022-08-06 15:38:24 +0900

    feat: add index, main, letter page

---
+++ components/InvitationLetter.tsx
@@ -0,0 +1,31 @@
+export const InvitationLetter = () => {
+  return (
+    <div className="text-center py-20 flex flex-col justify-center items-center">
+      <p className="mb-2">🌼</p>
+      <h2 className="mb-12">초 대 합 니 다</h2>
+      <p className="leading-6">
+        살랑이는 바람결에
+        <br />
+        사랑이 묻어나는 계절입니다.
+        <br />
+        여기 곱고 예쁜 두 사람이 사랑을 맺어
+        <br />
+        인생의 반려자가 되려 합니다
+        <br />
+        새 인생을 시작하는 이 자리에 오셔서
+        <br />
+        축복해 주시면 감사하겠습니다.
+        <br />
+      </p>
+      <div className="border-black border-r h-12 my-12" />
+      <div>
+        <p>
+          위동섭 • 정봉금 <span className="text-sm mr-2">의 장남</span> 성훈
+        </p>
+        <p>
+          조종호 • 홍선희 <span className="text-sm mr-2">의 장녀</span> 은비
+        </p>
+      </div>
+    </div>
+  );
+};
---
+++ components/MainPage.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import { useState } from "react";
+
+const texts = {
+  hoon: {
+    eng: "SeongHoon",
+    kor: "위성훈",
+  },
+  eunbee: {
+    eng: "Eunbee",
+    kor: "조은비",
+  },
+  and: {
+    eng: "and",
+    kor: "그리고",
+  },
+  p1: {
+    eng: "Sat 11am, October 1st, 2022",
+    kor: "2022년 10월 1일 토요일 오전 11시",
+  },
+  p2: {
+    eng: "Heeon Foret The Glass Garden",
+    kor: "희온포레 더 글라스가든",
+  },
+};
+
+export const MainPage = () => {
+  const [lang, setLang] = useState<"eng" | "kor">("eng");
+  return (
+    <div className="relative bg-stone-50">
+      <Image
+        src="/Main.jpeg"
+        width={390}
+        height={800}
+        objectFit="cover"
+        alt="환영합니다! Welcome!"
+      />
+      <div className="absolute top-24 w-full">
+        <div className="text-center">
+          <h1 className="text-3xl font-semibold">
+            {texts.hoon[lang]}
+            <span className="text-xl mx-2">{texts.and[lang]}</span>
+            {texts.eunbee[lang]}
+          </h1>
+          <div className="my-6">
+            <p className="text-xl">
+              {texts.p1[lang]}
+              <br />
+              {texts.p2[lang]}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-4 right-4 text-2xl flex gap-2">
+        <button onClick={() => setLang("kor")}>🇰🇷</button>
+        <button onClick={() => setLang("eng")}>🇬🇧</button>
+      </div>
+    </div>
+  );
+};
